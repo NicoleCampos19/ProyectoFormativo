012412424a1df50e22ef3026347b84d93cdd8a8d
Luz Gaspario <rebeca.monicoalfaro@gmail.com>
2024-07-19 11:18:23 -0600

    clase connexion

---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,28 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection?{
+        try {
+            //cambiar ip dependiendo de la computadora
+            val ipEmily = "jdbc:oracle:thin:@192.168.0.17:1521:xe"
+            val ipGabriela = "jdbc:oracle:thin:@192.168.1.13:1521:xe"
+            val ipprueba = "jdbc:oracle:thin:@10.10.1.119:1521:xe"
+
+            val usuario = "proyectoformativo24"
+            val contrasena = "GabEmi"
+
+            val conexion = DriverManager.getConnection(ipprueba, usuario, contrasena)
+
+
+
+            return conexion
+        }catch (e: Exception){
+            println("El error es este: $e")
+            return null
+        }
+    }
+}
